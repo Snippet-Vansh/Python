eb72dd3a90a5fb51da9daaee7657ba92da542d44
Ezzzz-Vansh🦖 <vanshvishwakarma1514@gmail.com>
2026-09-15 12:38:27 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,15 @@
+# name = 'vansh'
+# age = 27
+# name = 'aman'
+# age = 30
+# cgpa = 6.7
+# isStudent = True
+# print(name,age)
+# print(type(name))
+# print(type(age))
+# print(type(cgpa))
+# print(type(isStudent))
+
+# how to take input
+name = input('enter your name:')
+print('your name is' ,name)
